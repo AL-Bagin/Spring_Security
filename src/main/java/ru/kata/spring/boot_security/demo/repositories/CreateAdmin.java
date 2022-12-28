73405b70fe72a7c2d581569c5bed42cede67da91
AL-Bagin <mr.alex.bagin@mail.ru>
2022-12-28 14:18:10 +0300

    fix createadmin

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/CreateAdmin.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/CreateAdmin.java
@@ -20,6 +20,8 @@ public class CreateAdmin {
     @EventListener(ApplicationReadyEvent.class)
     public void createAdmin() {
         if (entityManager.find(User.class, (long)1) == null) {
+            entityManager.createNativeQuery("DELETE FROM roles WHERE id = 1").executeUpdate();
+            entityManager.createNativeQuery("DELETE FROM roles WHERE id = 2").executeUpdate();
             entityManager.createNativeQuery("INSERT INTO users (id, name, password, email) values (1, 'admin', '$2y$10$SL2.FVkvG2PQcbPTY8BOrebt7Ukgf2p0nw53gYl9UmHa1FxS24i0m', 'admin@mail.ru')").executeUpdate();
             entityManager.createNativeQuery("INSERT INTO roles (id, name) value (1, 'ROLE_ADMIN')").executeUpdate();
             entityManager.createNativeQuery("INSERT INTO roles (id, name) value (2, 'ROLE_USER')").executeUpdate();
